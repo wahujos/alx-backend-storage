dcceef228656c60c27c929bdd11535c484120d54
wahujos <jwahu30@gmail.com>
2024-05-10 03:10:52 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---Write a SQL script that lists all bands with Glam rock
+-- Write a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
 SELECT 
     band_name,
