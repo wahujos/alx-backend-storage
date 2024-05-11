6a72666740660314b208cd0f5c6ef784a4fc1c1d
wahujos <jwahu30@gmail.com>
2024-05-11 20:20:16 +0300

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,11 +3,12 @@
 -- Quantity in the table items can be negative.
 DELIMETER $$
 CREATE TRIGGER decreases_the_quantity_of_item
-AFTER INSERT ON order
+AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
     SET quantity = quantity - NEW.quantity_ordered
     WHERE item.id = NEW.item.id
 END
+$$
 DELIMETER ;
