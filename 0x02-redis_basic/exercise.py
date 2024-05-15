1b3a03d7d276c9b98893a5ddfe28f5951002a737
wahujos <jwahu30@gmail.com>
2024-05-16 01:33:39 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Writing strings to Redis
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -29,3 +29,37 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        In this exercise we will create a get method that take a key
+        string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back to the
+        desired format.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Also, implement 2 new methods: get_str and get_int that will
+        automatically parametrize Cache.get with the correct
+        conversion function.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: int) -> Union[int, None]:
+        """
+        Also, implement 2 new methods: get_str and get_int that will
+        automatically parametrize Cache.get with the correct
+        conversion function.
+        """
+        return self.get(key, fn=int)
