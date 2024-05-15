3a3494c4164fbf04e5759b5dcf58e785332e9268
wahujos <jwahu30@gmail.com>
2024-05-15 03:00:47 +0300

    11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that returns the list of school having a specific topic:
+"""
+
+from pymongo import MongoClient
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Prototype: def schools_by_topic(mongo_collection, topic):
+    mongo_collection will be the pymongo collection object
+    topic (string) will be topic searched
+    """
+    result = mongo_collection.find({"topic": topic})
+    return result
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["mydatabase"]
+    collection = db["mycollection"]
+    result = schools_by_topic(collection, topic = "topic")
+    client.close()
